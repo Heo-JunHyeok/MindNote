d4013cc38d6adfc28b0f5aecb59528c3721508dc
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-06-24 06:57:05 +0900

    +Model, register model

--- diarys/admin.py
+++ diarys/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Page
+
 
 # Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "content",
+    ]
