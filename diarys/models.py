53de22b6cfe225cbbc180aa56756f1a6368c5a91
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-06-24 06:59:40 +0900

    +comment

--- diarys/models.py
+++ diarys/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Page(models.Model):
+    # 제목, 내용, 감정 상태, 감정 점수, 작성일
     title = models.CharField(max_length=100)
     content = models.TextField()
     feeling = models.CharField(max_length=50)
