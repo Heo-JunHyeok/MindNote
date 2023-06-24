a401ad48ef523eb7e784a3ea7043cc7244d27722
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-06-24 11:06:02 +0900

    +page_list

--- diarys/views.py
+++ diarys/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Page
+
 
 # Create your views here.
+def page_list(request):
+    pages = Page.objects.all()
+    context = {"pages": pages}
+    return render(request, "diarys/page_list.html", context)
