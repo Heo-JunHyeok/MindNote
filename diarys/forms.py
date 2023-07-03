74277185c0c635b86707150b5e1b0df321767059
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-03 17:56:36 +0900

    Form에 css 적용하기

--- diarys/forms.py
+++ diarys/forms.py
@@ -5,4 +5,4 @@ from .models import Page
 class PageForm(forms.ModelForm):
     class Meta:
         model = Page
-        exclude = ("dt_created",)
+        fields = "__all__"
--- diarys/models.py
+++ diarys/models.py
@@ -7,9 +7,11 @@ class Page(models.Model):
     # 제목, 내용, 감정 상태, 감정 점수, 작성일
     title = models.CharField(max_length=100, validators=[validate_no_hash])
     content = models.TextField(validators=[validate_no_hash])
-    feeling = models.CharField(max_length=50, validators=[validate_no_numbers])
+    feeling = models.CharField(
+        max_length=50, validators=[validate_no_numbers, validate_no_hash]
+    )
     score = models.IntegerField(validators=[validate_score])
-    dt_created = models.DateField(auto_now_add=True)
+    dt_created = models.DateField()
 
     def __str__(self):
         return self.title
